10e2414d96b8a2be3dcf3dda428c9ee689e91345
cong.trinh <congtrinh@cong.trinh-ubuntu>
2020-12-28 21:20:42 +0700

    fix repository

--- library-management/src/main/java/aptech/library/management/repositories/AuthorRepository.java
+++ library-management/src/main/java/aptech/library/management/repositories/AuthorRepository.java
@@ -4,6 +4,8 @@ import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +31,20 @@ public class AuthorRepository implements IAuthorRepository {
 
     @Autowired
     private SessionFactory sessionFactory;
+    
+    public Session openSession() {
+    	Session session;
+
+    	try {
+    	    session = sessionFactory.getCurrentSession();
+    	} catch (HibernateException e) {
+    	    session = sessionFactory.openSession();
+    	}
+    	return session;
+    }
 
     public List < Author > getAuthors() {
-        Session session = sessionFactory.getCurrentSession();
+        Session session = openSession();
         CriteriaBuilder cb = session.getCriteriaBuilder();
         CriteriaQuery < Author > cq = cb.createQuery(Author.class);
         Root < Author > root = cq.from(Author.class);
@@ -41,19 +54,19 @@ public class AuthorRepository implements IAuthorRepository {
     }
 
     public void deleteAuthor(int id) {
-        Session session = sessionFactory.getCurrentSession();
+        Session session = openSession();
         Author book = session.byId(Author.class).load(id);
         session.delete(book);
     }
 
     public void saveAuthor(Author theAuthor) {
-        Session currentSession = sessionFactory.getCurrentSession();
-        currentSession.saveOrUpdate(theAuthor);
+        Session session = openSession();
+        session.saveOrUpdate(theAuthor);
     }
 
     public Author getAuthor(int theId) {
-        Session currentSession = sessionFactory.getCurrentSession();
-        Author theAuthor = currentSession.get(Author.class, theId);
+        Session session = openSession();
+        Author theAuthor = session.get(Author.class, theId);
         return theAuthor;
     }
 }
---
+++ library-management/target/library-management-0.0.1-SNAPSHOT/WEB-INF/views/home.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+ pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head><%@ page isELIgnored="false" %>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>javaguides.net</title>
+<link href="<c:url value="/resources/css/bootstrap.min.css" />"
+ rel="stylesheet">
+<script src="<c:url value="/resources/js/jquery-1.11.1.min.js" />"></script>
+<script src="<c:url value="/resources/js/bootstrap.min.js" />"></script>
+</head>
+ 
+<body>
+    <h1>${greeting}</h1>
+</body>
+ 
+ 
+</html>
